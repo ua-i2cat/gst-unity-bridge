b60b52b80d61b4f478cb46baba7e11f43d7fd77b
Xavi Artigas <xavier.artigas@i2cat.net>
2015-12-10 13:00:34 +0100

    Fix exports for Linux/Android

--- src/gub.h
+++ src/gub.h
@@ -6,7 +6,11 @@
 #ifndef __GUB_H__
 #define __GUB_H__
 
+#if _WIN32
 #define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API
+#endif
 
 void gub_copy_texture(const char *data, int w, int h, void *_TextureNativePtr);
 
