3a6193af0ea6f5b80f3869c162b246be575ed289
Xavi Artigas <xavier.artigas@i2cat.net>
2016-03-07 16:33:53 +0100

    Indicate pipeline in debug messages, so we can debug multiple-pipeline scenarios

--- src/gub_pipeline.c
+++ src/gub_pipeline.c
@@ -30,6 +30,18 @@ typedef struct _GUBPipeline {
 	float video_crop_bottom;
 } GUBPipeline;
 
+void gub_log_pipeline(GUBPipeline *pipeline, const char *format, ...)
+{
+	va_list argptr;
+	va_start(argptr, format);
+	gchar *new_format = g_strdup_printf("[pipeline 0x%p] %s", pipeline, format);
+	gchar *final_string = g_strdup_vprintf(new_format, argptr);
+	g_free(new_format);
+	va_end(argptr);
+	gub_log(final_string);
+	g_free(final_string);
+}
+
 EXPORT_API GUBPipeline *gub_pipeline_create()
 {
 	GUBPipeline *pipeline = (GUBPipeline *)malloc(sizeof(GUBPipeline));
@@ -67,9 +79,9 @@ EXPORT_API void gub_pipeline_play(GUBPipeline *pipeline)
 EXPORT_API void gub_pipeline_pause(GUBPipeline *pipeline)
 {
 	if (pipeline->pipeline) {
-		gub_log("Setting pipeline to PAUSED");
+		gub_log_pipeline(pipeline, "Setting pipeline to PAUSED");
 		gst_element_set_state(GST_ELEMENT(pipeline->pipeline), GST_STATE_PAUSED);
-		gub_log("State change completed");
+		gub_log_pipeline(pipeline, "State change completed");
 		pipeline->play_requested = FALSE;
 		pipeline->playing = FALSE;
 	}
@@ -78,9 +90,9 @@ EXPORT_API void gub_pipeline_pause(GUBPipeline *pipeline)
 EXPORT_API void gub_pipeline_stop(GUBPipeline *pipeline)
 {
 	if (pipeline->pipeline) {
-		gub_log("Setting pipeline to NULL");
+		gub_log_pipeline(pipeline, "Setting pipeline to NULL");
 		gst_element_set_state(GST_ELEMENT(pipeline->pipeline), GST_STATE_NULL);
-		gub_log("State change completed");
+		gub_log_pipeline(pipeline, "State change completed");
 	}
 }
 
@@ -98,14 +110,14 @@ static gboolean select_stream (GstBin *rtspsrc, guint num, GstCaps *caps, GUBPip
 {
 	gboolean select = (num == pipeline->video_index || num == pipeline->audio_index);
 	gchar *caps_str = gst_caps_to_string(caps);
-	gub_log("Found stream #%d (%s): %s", num, select ? "SELECTED" : "IGNORED", caps_str);
+	gub_log_pipeline(pipeline, "Found stream #%d (%s): %s", num, select ? "SELECTED" : "IGNORED", caps_str);
 	g_free(caps_str);
 	return select;
 }
 
 static void source_created(GstBin *playbin, GstElement *source, GUBPipeline *pipeline)
 {
-	gub_log("Setting properties to source %s", gst_plugin_feature_get_name(gst_element_get_factory(source)));
+	gub_log_pipeline(pipeline, "Setting properties to source %s", gst_plugin_feature_get_name(gst_element_get_factory(source)));
 	g_object_set(source, "latency", MAX_JITTERBUFFER_DELAY_MS, NULL);
 	g_object_set(source, "ntp-time-source", 3, NULL);
 	g_object_set(source, "buffer-mode", 4, NULL);
@@ -136,7 +148,7 @@ static GstPadProbeReturn pad_probe(GstPad *pad, GstPadProbeInfo *info, gpointer 
 		gst_query_set_context(query, context);
 		ret = GST_PAD_PROBE_HANDLED;
 
-		gub_log("Query for context type %s answered with context %p", context_type, context);
+		gub_log_pipeline(pipeline, "Query for context type %s answered with context %p", context_type, context);
 		gst_context_unref(context);
 	}
 beach:
@@ -157,17 +169,17 @@ EXPORT_API void gub_pipeline_setup(GUBPipeline *pipeline, const gchar *uri, int 
 	}
 
 	full_pipeline_description = g_strdup_printf("playbin uri=%s", uri);
-	gub_log("Using pipeline: %s", full_pipeline_description);
+	gub_log_pipeline(pipeline, "Using pipeline: %s", full_pipeline_description);
 
 	pipeline->pipeline = gst_parse_launch(full_pipeline_description, &err);
 	g_free(full_pipeline_description);
 	if (err) {
-		gub_log("Failed to create pipeline: %s", err->message);
+		gub_log_pipeline(pipeline, "Failed to create pipeline: %s", err->message);
 		return;
 	}
 
 	vsink = gst_parse_bin_from_description(gub_get_video_branch_description(), TRUE, NULL);
-	gub_log("Using video sink: %s", gub_get_video_branch_description());
+	gub_log_pipeline(pipeline, "Using video sink: %s", gub_get_video_branch_description());
 	g_object_set(pipeline->pipeline, "video-sink", vsink, NULL);
 	g_object_set(pipeline->pipeline, "flags", 0x0003, NULL);
 
@@ -179,7 +191,7 @@ EXPORT_API void gub_pipeline_setup(GUBPipeline *pipeline, const gchar *uri, int 
 			GstPad *pad = gst_element_get_static_pad(sink, "sink");
 			if (pad) {
 				gulong id = gst_pad_add_probe(pad, GST_PAD_PROBE_TYPE_QUERY_DOWNSTREAM, pad_probe, pipeline, NULL);
-				gub_log("Sink pad probe id is %d", id);
+				gub_log_pipeline(pipeline, "Sink pad probe id is %d", id);
 				gst_object_unref(pad);
 			}
 		}
@@ -190,7 +202,7 @@ EXPORT_API void gub_pipeline_setup(GUBPipeline *pipeline, const gchar *uri, int 
 	if (g_strstr_len(gub_get_video_branch_description(), -1, "videocrop") == NULL) {
 		pipeline->supports_cropping_blit = TRUE;
 	}
-	gub_log("Video branch %s cropping and blitting in one operation",
+	gub_log_pipeline(pipeline, "Video branch %s cropping and blitting in one operation",
 		pipeline->supports_cropping_blit ? "supports" : "does not support");
 
 	pipeline->video_index = video_index;
@@ -208,10 +220,10 @@ EXPORT_API void gub_pipeline_setup(GUBPipeline *pipeline, const gchar *uri, int 
 
 	if (net_clock_addr != NULL) {
 		gint64 start, stop;
-		gub_log("Trying to synchronize to network clock at %s %d", net_clock_addr, net_clock_port);
+		gub_log_pipeline(pipeline, "Trying to synchronize to network clock at %s %d", net_clock_addr, net_clock_port);
 		pipeline->net_clock = gst_net_client_clock_new("net_clock", net_clock_addr, net_clock_port, 0);
 		if (!pipeline->net_clock) {
-			gub_log("Could not create network clock at %s %d", net_clock_addr, net_clock_port);
+			gub_log_pipeline(pipeline, "Could not create network clock at %s %d", net_clock_addr, net_clock_port);
 			return;
 		}
 
@@ -219,9 +231,9 @@ EXPORT_API void gub_pipeline_setup(GUBPipeline *pipeline, const gchar *uri, int 
 		gst_clock_wait_for_sync(pipeline->net_clock, 30 * GST_SECOND);
 		stop = g_get_monotonic_time();
 		if (gst_clock_is_synced(pipeline->net_clock)) {
-			gub_log("Synchronized to network clock in %g seconds", (stop - start) / 1e6);
+			gub_log_pipeline(pipeline, "Synchronized to network clock in %g seconds", (stop - start) / 1e6);
 		} else {
-			gub_log("Could not synchronize to network clock after %g seconds", (stop - start) / 1e6);
+			gub_log_pipeline(pipeline, "Could not synchronize to network clock after %g seconds", (stop - start) / 1e6);
 		}
 
 		gst_pipeline_use_clock(GST_PIPELINE(pipeline->pipeline), pipeline->net_clock);
@@ -242,9 +254,9 @@ EXPORT_API gint32 gub_pipeline_grab_frame(GUBPipeline *pipeline, int *width, int
 	}
 
 	if (pipeline->playing == FALSE && pipeline->play_requested == TRUE) {
-		gub_log("Setting pipeline to PLAYING");
+		gub_log_pipeline(pipeline, "Setting pipeline to PLAYING");
 		gst_element_set_state(GST_ELEMENT(pipeline->pipeline), GST_STATE_PLAYING);
-		gub_log("State change completed");
+		gub_log_pipeline(pipeline, "State change completed");
 		pipeline->playing = TRUE;
 	}
 
@@ -254,21 +266,21 @@ EXPORT_API gint32 gub_pipeline_grab_frame(GUBPipeline *pipeline, int *width, int
 	}
 
 	if (!sink) {
-		gub_log("Pipeline does not contain a sink named 'sink'");
+		gub_log_pipeline(pipeline, "Pipeline does not contain a sink named 'sink'");
 		return 0;
 	}
 
 	g_object_get(sink, "last-sample", &pipeline->last_sample, NULL);
 	gst_object_unref(sink);
 	if (!pipeline->last_sample) {
-		gub_log("Could not read property 'last-sample' from sink %s",
+		gub_log_pipeline(pipeline, "Could not read property 'last-sample' from sink %s",
 			gst_plugin_feature_get_name(gst_element_get_factory(sink)));
 		return 0;
 	}
 
 	last_caps = gst_sample_get_caps(pipeline->last_sample);
 	if (!last_caps) {
-		gub_log("Sample contains no caps in sink %s",
+		gub_log_pipeline(pipeline, "Sample contains no caps in sink %s",
 			gst_plugin_feature_get_name(gst_element_get_factory(sink)));
 		gst_sample_unref(pipeline->last_sample);
 		pipeline->last_sample = NULL;
@@ -283,7 +295,7 @@ EXPORT_API gint32 gub_pipeline_grab_frame(GUBPipeline *pipeline, int *width, int
 		GstBuffer *buff = gst_sample_get_buffer(pipeline->last_sample);
 		GstClockTime pts = GST_BUFFER_PTS(buff);
 		GstClockTime curr = gst_clock_get_time(pipeline->net_clock) - gst_element_get_base_time(pipeline->pipeline);
-		gub_log("Buffer PTS is %" GST_TIME_FORMAT " and current is %" GST_TIME_FORMAT, GST_TIME_ARGS(pts), GST_TIME_ARGS(curr));
+		gub_log_pipeline(pipeline, "Buffer PTS is %" GST_TIME_FORMAT " and current is %" GST_TIME_FORMAT, GST_TIME_ARGS(pts), GST_TIME_ARGS(curr));
 	}
 #endif
 
