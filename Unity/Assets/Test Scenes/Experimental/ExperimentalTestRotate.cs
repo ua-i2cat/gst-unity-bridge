f7e7fa915272267baa8d82ab7b0a4eb833ebf65f
Xavi Artigas <xavier.artigas@i2cat.net>
2016-05-27 15:25:50 +0200

    Added Experimental test scenes (to demonstrate Experimental features)

---
+++ Unity/Assets/Test Scenes/Experimental/ExperimentalTestRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ExperimentalTestRotate : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(1, 0, 0), 1F);
+    }
+}
